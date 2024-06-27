f4c1f2ece82b43d9df81088a1ebf25560a550551
anirranjan <anirranjan@gmail.com>
2024-06-26 22:28:23 -0500

    nirranjan: switch chatbot back to 1.5 pro

--- server/routes/Chat.js
+++ server/routes/Chat.js
@@ -2,55 +2,54 @@ const express = require('express');
 const multer = require('multer')
 const upload = multer({ dest: "uploads/"})
 const router = express.Router();
-const { VertexAI } = require('@google-cloud/vertexai')
-const { Storage } = require('@google-cloud/storage')
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
+const { GoogleAIFileManager } = require('@google/generative-ai/files')
 
-const storage = new Storage();
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+const fileManager = new GoogleAIFileManager(apiKey);
 
-async function uploadFile(fileName, filePath){
-  const options = {
-    destination: fileName,
-  };
-
-  try {
-    await storage.bucket(process.env.BUCKET_NAME).upload(filePath, options)
-    console.log(`${filePath} uploaded`);
-    return `gs://${process.env.BUCKET_NAME}/${options.destination}`
-  } catch (error) {
-    console.log(error)
-  }
+async function uploadToGemini(path, mimeType){
+  const uploadResult = await fileManager.uploadFile(path, {
+    mimeType: mimeType,
+    displayName: path,
+  })
+  
+  console.log(`Uploaded file ${uploadResult.file.displayName} as: ${uploadResult.file.uri}`);
+  return uploadResult;
 }
 
-const vertex_ai = new VertexAI({project: process.env.PROJECT_ID, location: process.env.LOCATION})
-const generativeModel = vertex_ai.getGenerativeModel({
-  model: 'gemini-1.5-flash-001',
-  generationConfig: {
-    'maxOutputTokens': 8192,
-    'temperature': 1,
-    'topP': 0.95,
-  },
-  safetySettings: [
-    {
-        'category': 'HARM_CATEGORY_HATE_SPEECH',
-        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
-    },
-    {
-        'category': 'HARM_CATEGORY_DANGEROUS_CONTENT',
-        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
-    },
-    {
-        'category': 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
-        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
-    },
-    {
-        'category': 'HARM_CATEGORY_HARASSMENT',
-        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
-    }
-  ],
-  systemInstruction: {
-    parts: [{"text": `A fitness chatbot that provides encouraging responses in a concise and informative manner.`}]
-  },
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-pro-latest",
+  systemInstruction: " A fitness chatbot that provides encouraging responses in a concise and informative manner. ",
 });
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+];
 
 router.post('/chat', upload.single('doc'), async (req, res) => {
   const { userMessage } = req.body;
@@ -60,32 +59,22 @@ router.post('/chat', upload.single('doc'), async (req, res) => {
 
   if (req.file) {
     const doc = req.file;
-    const gcpUri = await uploadFile(doc.originalname, doc.path);
-
-    const filePart = {
-      fileData: {
-        fileUri: gcpUri,
-        mimeType: doc.mimetype,
+    const processedDoc = await uploadToGemini(doc.path, doc.mimetype);
+    
+    const result = await model.generateContent([
+      {
+        fileData: {
+          mimeType: processedDoc.file.mimeType,
+          fileUri: processedDoc.file.uri
+        }
       },
-    };
-
-    const textPart = {
-      text: userMessage,
-    }
-
-    const request = {
-      contents: [{role: 'user', parts: [filePart, textPart]}],
-    };
-
-    const result = await generativeModel.generateContent(request);
-    const aggregatedResult = await result.response;
-    res.json({ response: aggregatedResult.candidates[0].content.parts[0].text });
+      { text: userMessage },
+    ])
+    res.json({ response: result.response.text() });
   } else {
-    const result = await generativeModel.generateContent(userMessage);
-    const aggregatedResult = await result.response;
-    res.json({ response: aggregatedResult.candidates[0].content.parts[0].text });
+    const result = await model.generateContent(userMessage);
+    res.json({ response: result.response.text() });
   }
 });
 
 module.exports = router;
-
